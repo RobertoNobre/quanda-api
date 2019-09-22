bd3a00f4dddf7bf268b554395adea81e7e1ab202
betinhoow <robertonobre90@gmail.com>
2019-09-22 19:05:52 -0300

    Initial spring boot application

---
+++ src/main/java/com/betos/quanda/QuandaApplication.java
@@ -0,0 +1,13 @@
+package com.betos.quanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betos/quanda/QuandaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.betos.quanda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuandaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
