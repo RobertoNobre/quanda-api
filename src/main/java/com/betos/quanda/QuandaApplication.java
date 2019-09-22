7f73e1c050c99d442e3c7c6bc1e198a54d42bab4
betinhoow <robertonobre90@gmail.com>
2019-09-22 19:07:41 -0300

    Audit model to createdAt and updatedAt

--- src/main/java/com/betos/quanda/QuandaApplication.java
+++ src/main/java/com/betos/quanda/QuandaApplication.java
@@ -2,8 +2,10 @@ package com.betos.quanda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class QuandaApplication {
 
 	public static void main(String[] args) {
